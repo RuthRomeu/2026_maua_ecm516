e8855c1b36b4e0cf3bd3e9cadc71696c8b525ad7
Ruth Romeu <ruthromeu2@gmail.com>
2026-02-28 08:45:44 -0300

    first commit

---
+++ intro_js/index.js
@@ -0,0 +1,25 @@
+//console.log ("Hello World")
+// //declarando constantes
+// const nome = 'Ruth'
+// console.log(nome)
+// const idade = 22
+// console.log(typeof(idade))
+
+// //diferença entre let e const: let admite reatribuição
+// let a =2
+// let passouDeAno = true
+// console.log(passouDeAno)
+
+
+/* const n1 = 2
+const n2 = '3'
+const n3 = n1+n2
+console.log(n3)
+
+const n4 = n1 + Number(n2)
+console.log(n4) */
+
+/* console.log(1 == '1')
+console.log(1 === '1') */
+
+// evitar usar o '==', usar '===' no lugar
